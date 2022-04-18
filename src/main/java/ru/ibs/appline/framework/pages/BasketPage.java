def4f78236b8f32c940d25396245b0554e67cb88
nerfikys <oryznov@mail.ru>
2022-04-18 21:20:06 +0300

    Last improvs

--- src/main/java/ru/ibs/appline/framework/pages/BasketPage.java
+++ src/main/java/ru/ibs/appline/framework/pages/BasketPage.java
@@ -8,11 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.ibs.appline.framework.data.Product;
-import ru.ibs.appline.framework.utils.PropsConst;
 import ru.ibs.appline.framework.utils.Utils;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,7 +20,7 @@ public class BasketPage extends BasePage {
     @FindBy(xpath = "//span[contains(text(),'Добавить компанию')]")
     WebElement alert;
 
-    @FindBy(xpath = "//div[contains(@id,'split-Main')]/div/div/a/span")
+    @FindBy(xpath = "//span[contains(text(), 'избранное')]/../../../../../../../a/span[1]")
     List<WebElement> mainListTitle;
 
     @FindBy(xpath = "//section[@data-widget='total']/div/div/div/span")
@@ -45,23 +43,22 @@ public class BasketPage extends BasePage {
         Collections.sort(productList);
         List<Product> productList2 = new ArrayList<>();
         for (WebElement x : mainListTitle) {
-                productList2.add(new Product(x.getText(),
-                        Utils.convertToInteger(x.findElement(By.xpath("./../../../div[3]//span")).getText())));
-            }
+            productList2.add(new Product(x.getText(),
+                    Utils.convertToInteger(x.findElement(By.xpath("./../../../div[3]//span")).getText())));
+        }
         Collections.sort(productList2);
         ArrayList<Product> productArrayList3 = new ArrayList<>();
         ArrayList<Product> productArrayList4 = new ArrayList<>();
         if (!productList.equals(productList2)) {
             productArrayList3.addAll(productList);
             productArrayList3.removeAll(productList2);
-            if(productList.size()==productList2.size())
-            {
+            if (productList.size() == productList2.size()) {
                 productArrayList4.addAll(productList2);
                 productArrayList4.removeAll(productList);
-            Assertions.fail("Со страницы не получилось собрать идентичный список продуктов, найденные различия: \nПервый список: "+productArrayList3+ "\nВторой список: "+productArrayList4);
+                Assertions.fail("Со страницы не получилось собрать идентичный список продуктов, найденные различия: \nПервый список: " + productArrayList3 + "\nВторой список: " + productArrayList4);
             }
         }
-        Assertions.assertTrue(productArrayList3.isEmpty(), "Со страницы не получилось собрать идентичный список продуктов, не были найдены(вероятно они были удалены): "+productArrayList3);
+        Assertions.assertTrue(productArrayList3.isEmpty(), "Со страницы не получилось собрать идентичный список продуктов, не были найдены(вероятно они были удалены): " + productArrayList3);
         return this;
     }
 
@@ -76,7 +73,7 @@ public class BasketPage extends BasePage {
     @Step("Удалить всё из корзины")
     public BasketPage dellAllFromBasket() {
         waitToClickable(dell).click();
-        waitToClickable(driverManager.getDriver().findElement(By.xpath("//div[contains(text(),'Удаление товаров')]/..//button"))).click();
+        waitToClickable(driverManager.getDriver().findElement(By.xpath("//div[contains(text(),'Удалить товар')]/..//button"))).click();
         return this;
     }
 
